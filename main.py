fdad7d899db08b1a301c5eeee43ec15ce4457665
Jonnester <84779222+jon123boss@users.noreply.github.com>
2024-07-12 08:52:33 +0800

    Making the game board

--- main.py
+++ main.py
@@ -4,12 +4,20 @@ import sys
 pygame.init()
 
 SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_HEIGHT = 800
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+GRAY = (150, 150, 150)
+RADIUS = 10
+PADDING = 20
+NUM_ROWS = 5
+NUM_COLS = 5
+
+square_width = (SCREEN_WIDTH - (NUM_COLS + 1) * PADDING) // NUM_COLS
+square_height = (SCREEN_HEIGHT - (NUM_ROWS + 1) * PADDING) // NUM_ROWS
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Multiplying game")
+pygame.display.set_caption("Multiplying Game")
 
 running = True
 while running:
@@ -19,8 +27,16 @@ while running:
 
     screen.fill(BLACK)
 
+    for row in range(NUM_ROWS):
+        for col in range(NUM_COLS):
+            x = PADDING + col * (square_width + PADDING)
+            y = PADDING + row * (square_height + PADDING)
+
+            pygame.draw.rect(screen, GRAY, (x, y, square_width, square_height), border_radius=RADIUS)
+
     pygame.display.flip()
     pygame.time.Clock().tick(60)
 
 pygame.quit()
 sys.exit()
+
